a3d1b0c6bfc51aac88dae7e6e4c24cc8339922ac
cristina-ulinici <cristina4334@gmail.com>
2019-02-22 12:51:41 +0200

    argparse

--- munzip.py
+++ munzip.py
@@ -2,10 +2,14 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", help="number of threads", type=int)
-parser.add_argument("-log", nargs='?', help="logfile name")  # numele optional -
-parser.add_argument("-ttimeout", help="ttimeout in sec for a zip file", type=int)
-parser.add_argument("-gtimeout", help="total ttimeout in sec", type=int)
+parser.add_argument("-t", help="number of threads", type=int, default=3)
+parser.add_argument("-log", nargs='?', help="logfile name", default="log.txt")
+parser.add_argument("-ttimeout", help="ttimeout in sec for a zip file", type=int, default=60)
+parser.add_argument("-gtimeout", help="total ttimeout in sec", type=int, default=180)
 parser.add_argument("dir", help="dir with zip files")
 
 args = parser.parse_args()
+args.log = args.log if args.log!=None else "log.txt"
+
+print(args.t)
+print(args.log)
