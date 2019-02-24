c9f4cd1bada08aef09ccb76c2b978fb6f3f65199
cristina-ulinici <cristina4334@gmail.com>
2019-02-25 01:40:22 +0200

    ftpsync ftp

--- ftpsync.py
+++ ftpsync.py
@@ -1,3 +1,5 @@
+# Status Failed doar cand a expirat timeout
+
 import argparse
 import configparser
 import logging
@@ -6,6 +8,7 @@ from multiprocessing import *
 import os
 import time
 import ftputil
+import ftputil.session
 import shutil
 
 parser = argparse.ArgumentParser()
@@ -21,25 +24,75 @@ log_file = config['General']['log_path']
 logging.basicConfig(filename=log_file, format='[%(asctime)s]\t[%(levelname)s]\t\t[line:%(lineno)d]\t%(message)s', level=logging.DEBUG)
 logging.info('Config file: %s', args.config)
 
-
-def sync(i):
-    sync_nr = 'Sync_#'+ str(i+1)
-    logging.debug('Thread %i started with %s', i, sync_nr)
-    # src_type = config[sync_nr]['src_type']
-    # src_name = config[sync_nr]['src_name']
-    src = config[sync_nr]['src']
-    # dst_type = config[sync_nr]['dst_type']
-    dst = config[sync_nr]['dst']
-    # dst_name = config[sync_nr]['dst_name']
-    # ftp_mode = config[sync_nr]['ftp_mode']
-
-    if (config[sync_nr]['delete_extra_dst_content'] == 'YES' and config[sync_nr]['move_files'] == 'YES'):
-        logging.error('Inconsistency in config file in [%s]: delete_extra_dst_content and move_files', sync_nr)
-        return
-
+def ftp_sync(src, dst, sync_nr):
     src_files = []
+    src = src.split()
+    try:
+        if(config[sync_nr]['ftp_mode'] == 'PASSIVE'):
+            my_session_factory = ftputil.session.session_factory(use_passive_mode=True)
+        else:
+            my_session_factory = ftputil.session.session_factory(use_passive_mode=False)
 
-    # if src_type = dst_type = DISK
+        with ftputil.FTPHost(src[0], src[1], src[2],session_factory=my_session_factory) as host:
+            for root, dirs, files in host.walk('/'):
+                for d in dirs:
+                    src_dir = host.path.join(root, d)
+                    src_files.append(src_dir[len(src)-2:])
+                    s_modif = host.stat(src_dir).st_mtime
+                    dest_dir = os.path.join(dst, src_dir[len(src)-2:].replace('/', '\\'))
+                    if (os.path.exists(dest_dir)):
+                        d_modif = os.stat(dest_dir).st_mtime
+                        logging.info('%s: Directory %s already exists', sync_nr, src_dir[len(src):])
+                    else:
+                        try:
+                            os.makedirs(dest_dir)
+                            logging.info('%s: Directory %s was created', sync_nr, dest_dir)
+                        except Exception as e:
+                            logging.error('%s: Unable to create directory %s: %s', sync_nr, dest_dir, e)
+                for f in files:
+                    src_file = host.path.join(root, f)
+                    src_files.append(src_file[len(src)+1:])
+                    s_modif = host.stat(src_file).st_mtime
+                    dest_file = os.path.join(dst, src_file[len(src)-2:].replace('/', '\\'))
+                    if (os.path.isfile(dest_file)):
+                        d_modif = os.stat(dest_file).st_mtime
+                    else:
+                        d_modif = 0
+
+                    if(int(s_modif)==int(d_modif)):
+                        logging.info('%s: File %s: nothing changed', sync_nr, src_file[len(src)-2:])
+                    else:
+                        logging.info('%s: File %s has changed in the source', sync_nr, src_file[len(src)-2:])
+                        try:
+                            # shutil.copy2(src_file, dest_file[:len(dest_file)-len(f)])
+                            host.download(src_file, dest_file)
+                            logging.info('%s: File %s is up to date', sync_nr, src_file[len(src)-2:])
+                        except Exception as e:
+                            logging.error('%s: Unable to copy %s: %s', sync_nr, src_file, e)
+
+        if(config[sync_nr]['move_files'] == 'YES'):
+            for i in host.listdir(src):
+                item = host.path.join(src, i)
+                if(host.path.isfile(item)):
+                    try:
+                        host.remove(item)
+                        logging.info('%s: %s deleted', sync_nr, item)
+                    except Exception as e:
+                        logging.error('%s: Unable to delete file: %s: %s', sync_nr, item, e)
+                if(host.path.isdir(item)):
+                    try:
+                        host.rmtree(item)
+                        logging.info('%s: %s deleted', sync_nr, item)
+                    except Exception as e:
+                        logging('%s: Unable to delete directory %s: %s', sync_nr, item, e)
+
+    except Exception as e:
+        logging.error('%s: %s', sync_nr, e)
+    return src_files
+
+
+def disk_sync(src, dst, sync_nr):
+    src_files = []
     for root, dirs, files in os.walk(src):
         for d in dirs:
             src_dir = os.path.join(root, d)
@@ -76,7 +129,6 @@ def sync(i):
                 except Exception as e:
                     logging.error('%s: Unable to copy %s: %s', sync_nr, src_file, e)
 
-
     if(config[sync_nr]['move_files'] == 'YES'):
         for i in os.listdir(src):
             item = os.path.join(src, i)
@@ -92,6 +144,23 @@ def sync(i):
                     logging.info('%s: %s deleted', sync_nr, item)
                 except Exception as e:
                     logging('%s: Unable to delete directory %s: %s', sync_nr, item, e)
+
+    return src_files
+
+def sync(i):
+    sync_nr = 'Sync_#'+ str(i+1)
+    logging.debug('Thread %i started with %s', i, sync_nr)
+    src = config[sync_nr]['src']
+    dst = config[sync_nr]['dst']
+
+    if (config[sync_nr]['delete_extra_dst_content'] == 'YES' and config[sync_nr]['move_files'] == 'YES'):
+        logging.error('Inconsistency in config file in [%s]: delete_extra_dst_content and move_files', sync_nr)
+        return
+
+    if (config[sync_nr]['src_type'] == 'DISK'):
+        src_files = disk_sync(src, dst, sync_nr)
+    else:
+        src_files = ftp_sync(src, dst, sync_nr)
 
     if(config[sync_nr]['delete_extra_dst_content'] == 'YES'):
         for root, dirs, files in os.walk(dst):
@@ -150,11 +219,3 @@ if __name__ == "__main__":
         except Exception as e:
             logging.error('Thread %d: %s', i, e)
             continue
-
-
-
-
-# with ftputil.FTPHost('ftp.dlptest.com', 'dlpuser@dlptest.com', 'puTeT3Yei1IJ4UYT7q0r') as host:
-#     for root, dirs, files in host.walk('/'):
-#         for f in files:
-#             print(f)
